79fe3e0ce58744383686fc81613853a3c47cce6a
Wellijr <101907715+Wellijr@users.noreply.github.com>
2022-04-08 21:39:42 -0300

    Create Math random.java

---
+++ Math random.java
@@ -0,0 +1,11 @@
+import java.lang.Math;
+import java.util.Random;
+
+
+class Main {
+  public static void main(String[] args) {
+   for (int i=0;i<60 ; i++){
+   System.out.println(Math.round(Math.random()*(50 - (-50))+ (-50)) +" ");
+   }
+  }
+}
